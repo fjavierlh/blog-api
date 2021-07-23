9451482c05944d3bbdba49d26e80cab1f7313048
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2021-07-23 09:19:57 +0200

    test: added test to create offensive word use case

---
+++ src/application/use-cases/create-offensive-word.use-case.spec.ts
@@ -0,0 +1,25 @@
+jest.mock('./../../infrastructure/repositories/offensive-word.repository.mongo');
+
+import 'reflect-metadata';
+import Container from 'typedi';
+import {CreateOffensiveWordUseCase} from './create-offensive-word.use-case';
+import { OffensiveWordRequest} from './offensive-word.request';
+import { OffensiveWordRepositoryMongo } from './../../infrastructure/repositories/offensive-word.repository.mongo';
+
+describe('CreateOffensiveWordUseCase', () => {
+	it('should create an offensive word and persit it', () => {
+		const repository = new OffensiveWordRepositoryMongo();
+
+		Container.set('OffensiveWordRepository', repository);
+        
+		const useCase = Container.get(CreateOffensiveWordUseCase);
+		const offensiveWordRequest: OffensiveWordRequest = {
+			word: 'Test',
+			level: 1
+		};
+
+		useCase.execute(offensiveWordRequest);
+		expect(repository.save).toBeCalled();
+
+	});
+});
