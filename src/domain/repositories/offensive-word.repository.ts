c0c9b5398c4fdf4f83b5b6e7ad8f31f2f4a58987
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2021-07-23 09:13:29 +0200

    feat: created offensive word data transfer interface

---
+++ src/domain/repositories/offensive-word.repository.ts
@@ -0,0 +1,16 @@
+import { OffensiveWord } from '../entities/offensive-word.entity';
+import { IdVO } from '../vos/id.vo';
+
+export interface OffensiveWordRepository {
+    
+    save(offensiveWord: OffensiveWord): void;
+    
+    delete(idOffensiveWord: IdVO): void;
+    
+    showAll(): Promise<OffensiveWord[]>;
+
+    // showById(idOffensiveWord: IdVO): Promise<OffensiveWord>;
+
+    // update(idOffensiveWord: IdVO, offensiveWord: OffensiveWord): Promise<OffensiveWord>;
+
+}
