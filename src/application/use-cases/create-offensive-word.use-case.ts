6684155f15b70bf378cb9f92c5af3e04f31b7832
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2021-07-23 09:17:27 +0200

    feat: created use case to create an offensive word

---
+++ src/application/use-cases/create-offensive-word.use-case.ts
@@ -0,0 +1,25 @@
+import { Service } from 'typedi';
+import { OffensiveWordType } from '../../domain/entities/offensive-word.entity';
+import { OffensiveWordService } from '../../domain/services/offensive-word.service';
+import { IdVO } from '../../domain/vos/id.vo';
+import { LevelVO } from '../../domain/vos/level.vo';
+import { WordVO } from '../../domain/vos/word.vo';
+import { OffensiveWordRequest } from './offensive-word.request';
+
+@Service()
+export class CreateOffensiveWordUseCase {
+
+	constructor(private offensiveWordService: OffensiveWordService) {}
+
+	execute(offensiveWordRequest: OffensiveWordRequest): void {
+        
+		const offensiveWordData: OffensiveWordType = {
+			id: IdVO.create(),
+			word: WordVO.create(offensiveWordRequest.word),
+			level: LevelVO.create(offensiveWordRequest.level)
+		};
+		
+		this.offensiveWordService.persist(offensiveWordData);
+	}
+
+}
