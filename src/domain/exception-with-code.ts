9d0445f5c1b652a8b467ebc4c2d1571c129b86f6
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2021-07-26 13:51:54 +0200

    chore: created custom exception with respone status code and message

---
+++ src/domain/exception-with-code.ts
@@ -0,0 +1,9 @@
+import { CustomError } from 'ts-custom-error';
+
+export class ExceptionWithCode extends CustomError {
+
+	public constructor(public code: number, message: string) {
+		super(message);
+	}
+
+}
