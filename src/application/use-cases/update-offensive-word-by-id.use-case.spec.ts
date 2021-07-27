a354a388aad16adbef2770dfd0472fb5da050bd4
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2021-07-27 12:57:09 +0200

    test: added new fixtures to test suite

--- src/application/use-cases/update-offensive-word-by-id.use-case.spec.ts
+++ src/application/use-cases/update-offensive-word-by-id.use-case.spec.ts
@@ -1,5 +1,9 @@
 import 'reflect-metadata';
 import Container from 'typedi';
+import { OffensiveWord } from '../../domain/entities/offensive-word.entity';
+import { IdVO } from '../../domain/vos/id.vo';
+import { LevelVO } from '../../domain/vos/level.vo';
+import { WordVO } from '../../domain/vos/word.vo';
 import { OffensiveWordRepositoryMongo } from '../../infrastructure/repositories/offensive-word.repository.mongo';
 import { UpdateOffensiveWordByIdUseCase } from './update-offensive-word-by-id.use-case';
 
@@ -7,13 +11,9 @@ jest.mock('./../../infrastructure/repositories/offensive-word.repository.mongo',
 	return {
 		OffensiveWordRepositoryMongo: jest.fn().mockImplementation(() => {
 			return {
-				update: jest.fn().mockImplementation(() => [
-					{ id: 'cf1e0d44-7b6a-4a0e-843c-cbc0e0b4a668', word: 'Test1', level: 1 },
-					{ id: 'f2dd593e-af8e-4754-bed3-b42d2cfce636', word: 'Test2', level: 2 },
-					{ id: '72cc472f-a06b-44aa-a1cc-8ca5d353e095', word: 'Test3', level: 3 },
-					{ id: 'e86c4e2b-c26f-49f2-ac92-5c7e32784719', word: 'Test4', level: 4 },
-					{ id: '50ff8d46-9ab4-4610-84b8-969e9d95bd17', word: 'Test5', level: 5 },
-				])
+				update: jest.fn().mockImplementation(() => 
+					new OffensiveWord({ id: IdVO.createWithUUID('f2dd593e-af8e-4754-bed3-b42d2cfce636'), word: WordVO.create('Tested'), level: LevelVO.create(1) })
+				)
 			};
 		})
 	};
