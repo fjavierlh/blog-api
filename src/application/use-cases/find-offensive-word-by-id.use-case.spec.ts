0348ff15c9e29d65bd3ce2039088c0910e37a846
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2021-07-27 11:46:48 +0200

    test: added test suite to find offensive word use case

---
+++ src/application/use-cases/find-offensive-word-by-id.use-case.spec.ts
@@ -0,0 +1,37 @@
+import 'reflect-metadata';
+import Container from 'typedi';
+import { OffensiveWord } from '../../domain/entities/offensive-word.entity';
+import { IdVO } from '../../domain/vos/id.vo';
+import { LevelVO } from '../../domain/vos/level.vo';
+import { WordVO } from '../../domain/vos/word.vo';
+import { OffensiveWordRepositoryMongo } from '../../infrastructure/repositories/offensive-word.repository.mongo';
+import { IdRequest } from './id.request';
+import { FindOffensiveWordById } from './find-offensive-word-by-id.use-case';
+
+jest.mock('./../../infrastructure/repositories/offensive-word.repository.mongo', () => {
+	return {
+		OffensiveWordRepositoryMongo: jest.fn().mockImplementation(() => {
+			return {
+				showById: jest.fn().mockImplementation(() => 
+					new OffensiveWord({id: IdVO.create(), word: WordVO.create('Test1'), level: LevelVO.create(1)})
+				)
+			};
+		})
+	};
+});
+
+describe('FindOffensiveWordById Test Suite', () => {
+	it('should return a offensive word by id', async () => {
+		const repository = new OffensiveWordRepositoryMongo();
+		Container.set('OffensiveWordRepository', repository);
+		const useCase: FindOffensiveWordById = Container.get(FindOffensiveWordById);
+		const testId: IdRequest = 'f2dd593e-af8e-4754-bed3-b42d2cfce636';
+        
+		const offensiveWord = await useCase.execute(testId);
+		
+		expect(offensiveWord.id).toEqual(testId);
+		expect(offensiveWord.word).toEqual('Test1');
+		expect(offensiveWord.level).toEqual(1);
+        
+	});
+});
