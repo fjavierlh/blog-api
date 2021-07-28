fa4c200699f0652b014f4bf026f5fee5c61d916c
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2021-07-28 18:44:59 +0200

    chore: added imports and setted user repository in DI container

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,14 @@ import { json } from 'body-parser';
 import { offensiveWordRouter } from './infrastructure/routes/offensive-word.route';
 import Container from 'typedi';
 import { OffensiveWordRepositoryMongo } from './infrastructure/repositories/offensive-word.repository.mongo';
+import './infrastructure/config/postgres';
+import { authRouter } from './infrastructure/routes/auth.route';
+import { UserRepositoryPostgres } from './infrastructure/repositories/user.repository.pg';
+import passport from 'passport';
+import passportMiddelware from './infrastructure/middlewares/pasport';
 
-Container.set('OffensiveWordRepository', new OffensiveWordRepositoryMongo);
+Container.set('OffensiveWordRepository', new OffensiveWordRepositoryMongo());
+Container.set('UserRepository', new UserRepositoryPostgres());
 
 connectToDB();
 
@@ -17,7 +23,9 @@ const app: Application = express();
 
 app.use(json());
 app.use(offensiveWordRouter);
-
+app.use(authRouter);
+app.use(passport.initialize());
+passport.use(passportMiddelware);
 
 
 export default app;
