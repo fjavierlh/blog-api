0d7ed6c85700492f2b0fa526b72e58268978a7b5
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2021-07-28 18:40:59 +0200

    chore: defined user model to sequelize ORM

---
+++ src/infrastructure/repositories/user.schema.ts
@@ -0,0 +1,21 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/postgres';
+
+const UserModel = sequelize.define('users', {
+	id: {
+		type: DataTypes.UUID,
+		allowNull: false,
+		primaryKey: true
+	},
+	email: {
+		type: DataTypes.STRING,
+		allowNull: false,
+		unique: true
+	},
+	password: {
+		type: DataTypes.STRING,
+		allowNull: false
+	}
+});
+
+export { UserModel };
