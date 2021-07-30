543745fdf24b7150a7c123e5145d938d19d17817
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2021-07-30 12:58:05 +0200

    test: updated test with new property role

--- src/domain/entities/user.entity.spec.ts
+++ src/domain/entities/user.entity.spec.ts
@@ -1,5 +1,6 @@
 import { EmailVO } from '../vos/auth-user/email.vo';
 import { PasswordVO } from '../vos/auth-user/password.vo';
+import { Role, RoleVO } from '../vos/auth-user/role.vo';
 import { IdVO } from '../vos/id.vo';
 import { User, UserType } from './user.entity';
 describe('User test suite', () => {
@@ -7,11 +8,13 @@ describe('User test suite', () => {
 		const validUUID = '3b37664b-7292-48ba-a986-55480402d683';
 		const validEmail = 'hi@mymail.com';
 		const validPassword = '@Bcd3fgh1#';
+		const validRole = 'USER';
 
 		const userData: UserType = {
 			id: IdVO.createWithUUID(validUUID),
 			email: EmailVO.create(validEmail),
-			password: PasswordVO.create(validPassword)
+			password: PasswordVO.create(validPassword),
+			role: RoleVO.create(Role.USER)
 		};
 
 		const expectedUserEntity = new User(userData);
@@ -19,6 +22,7 @@ describe('User test suite', () => {
 		expect(expectedUserEntity.id.value).toBe(validUUID);
 		expect(expectedUserEntity.email.value).toBe(validEmail);
 		expect(expectedUserEntity.password.value).toBe(validPassword);
+		expect(expectedUserEntity.role.value).toBe(validRole);
         
 	});
 });
