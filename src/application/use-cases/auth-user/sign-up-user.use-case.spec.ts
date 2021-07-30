e9714a11c5c737b52bfe936207cda69d14b420df
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2021-07-30 09:56:16 +0200

    test: added new test to sign up user use case

--- src/application/use-cases/auth-user/sign-up-user.use-case.spec.ts
+++ src/application/use-cases/auth-user/sign-up-user.use-case.spec.ts
@@ -4,11 +4,21 @@ import { UserRepositoryPostgres } from '../../../infrastructure/repositories/use
 import { SignUpUserUseCase } from './sign-up-user.use-case';
 import { SignUpRequest } from './sign-up.request';
 
-jest.mock('./../../../infrastructure/repositories/user.repository.pg');
+//jest.mock('./../../../infrastructure/repositories/user.repository.pg');
+
+jest.mock('./../../../infrastructure/repositories/user.repository.pg', () => {
+	return {
+		UserRepositoryPostgres: jest.fn().mockImplementation(() => {
+			return { saveUser: jest.fn() };
+		})
+	};
+});
 
 describe('SignUpUserUseCase test suite', () => {
 	it('should sign up an user', async () => {
+
 		const repository = new UserRepositoryPostgres();
+
 		Container.set('UserRepository', repository);
 
 		const useCase = Container.get(SignUpUserUseCase);
@@ -20,5 +30,6 @@ describe('SignUpUserUseCase test suite', () => {
 
 		await useCase.execute(userTest);
 		expect(repository.saveUser).toBeCalled();
+
 	});
 });
