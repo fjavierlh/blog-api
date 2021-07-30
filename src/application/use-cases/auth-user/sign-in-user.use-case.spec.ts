d4509cf68008d0bfb7e78ec67be50a2604fe2235
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2021-07-30 12:58:38 +0200

    test: updated test with new user property role

--- src/application/use-cases/auth-user/sign-in-user.use-case.spec.ts
+++ src/application/use-cases/auth-user/sign-in-user.use-case.spec.ts
@@ -3,6 +3,7 @@ import Container from 'typedi';
 import { User } from '../../../domain/entities/user.entity';
 import { EmailVO } from '../../../domain/vos/auth-user/email.vo';
 import { PasswordVO } from '../../../domain/vos/auth-user/password.vo';
+import { Role, RoleVO } from '../../../domain/vos/auth-user/role.vo';
 import { IdVO } from '../../../domain/vos/id.vo';
 import { UserRepositoryPostgres } from '../../../infrastructure/repositories/user.repository.pg';
 import { SignInUserUseCase } from './sign-in-user.use-case';
@@ -16,7 +17,9 @@ jest.mock('./../../../infrastructure/repositories/user.repository.pg', () => {
 					return new User({
 						id: IdVO.createWithUUID('f2dd593e-af8e-4754-bed3-b42d2cfce636'),
 						email: EmailVO.create('hi@mymail.com'),
-						password: PasswordVO.create('$2b$10$Evl2ukNLSMrqpUB4vjZNCOEkAG3LAdXzOwWhHKtw.KRvW5Grn0NlW')});
+						password: PasswordVO.create('$2b$10$Evl2ukNLSMrqpUB4vjZNCOEkAG3LAdXzOwWhHKtw.KRvW5Grn0NlW'),
+						role: RoleVO.create(Role.USER)
+					});
 				})
 			};
 		})
