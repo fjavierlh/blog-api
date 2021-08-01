ab9e00b253c054084cc6b3a58e58fcde65b2e628
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2021-08-01 12:51:05 +0200

    chore: initialized databases and populate it in main execution file

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,4 @@
-
 import 'reflect-metadata';
-import { connectToDB } from './infrastructure/config/mongo';
 import express, { Application } from 'express';
 import { json } from 'body-parser';
 import { offensiveWordRouter } from './infrastructure/routes/offensive-word.route';
@@ -11,11 +9,18 @@ import { authRouter } from './infrastructure/routes/auth.route';
 import { UserRepositoryPostgres } from './infrastructure/repositories/user.repository.pg';
 import passport from 'passport';
 import passportMiddelware from './infrastructure/middlewares/pasport';
+import { connectToDB as connectToOffensiveWordsDB } from './infrastructure/config/mongo';
+import { connectToUsersdDB } from './infrastructure/config/postgres';
+import { populateDatabase as populateUsersDatabase } from './infrastructure/config/populate';
 
 Container.set('OffensiveWordRepository', new OffensiveWordRepositoryMongo());
 Container.set('UserRepository', new UserRepositoryPostgres());
 
-connectToDB();
+(async () => {
+	await connectToOffensiveWordsDB();
+	await connectToUsersdDB();
+	await populateUsersDatabase();
+})();
 
 console.log('App running');
 
@@ -26,6 +31,5 @@ app.use(offensiveWordRouter);
 app.use(authRouter);
 app.use(passport.initialize());
 passport.use(passportMiddelware);
-
 
 export default app;
