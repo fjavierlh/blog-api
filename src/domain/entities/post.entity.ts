db85088d7c0b191f918b5bebb1e767ed2b25649b
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2021-08-02 13:46:19 +0200

    chore: created post entity

---
+++ src/domain/entities/post.entity.ts
@@ -0,0 +1,45 @@
+import { AuthorNameVO } from '../vos/author/author-name.vo';
+import { AuthorNicknameVO } from '../vos/author/author-nickname.vo';
+import { IdVO } from '../vos/id.vo';
+import { PostContentVO } from '../vos/posts/text.vo';
+import { PostTitleVO } from '../vos/posts/title.vo';
+import { CommentPost } from './comment-post.entity';
+
+export type PostType = {
+    id: IdVO,
+    author: AuthorNameVO,
+    nickname: AuthorNicknameVO,
+    title: PostTitleVO,
+    content: PostContentVO,
+    comments: CommentPost[]
+}
+
+export class Post {
+
+	constructor(private post: PostType) { }
+
+	get id(): IdVO {
+		return this.post.id;
+	}
+
+	get author(): AuthorNameVO {
+		return this.post.author;
+	}
+
+	get nickname(): AuthorNicknameVO {
+		return this.post.nickname;
+	}
+
+	get title(): PostTitleVO {
+		return this.post.title;
+	}
+
+	get content(): PostContentVO {
+		return this.post.content;
+	}
+
+	get comments():CommentPost[] {
+		return this.post.comments;
+	} 
+
+}
