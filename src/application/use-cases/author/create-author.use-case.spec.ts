b6ea0db6cbec9a728de6e980a3ff6ee1b90f2b2e
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2021-08-03 10:49:31 +0200

    chore: created use case to create an author

---
+++ src/application/use-cases/author/create-author.use-case.spec.ts
@@ -0,0 +1,36 @@
+import 'reflect-metadata';
+import Container from 'typedi';
+import { AuthorRepositoryMongo } from '../../../infrastructure/repositories/author.repository.mongo';
+import { AuthorRequest } from './author.request';
+import { CreateAuthorUseCase } from './create-author.use-case';
+
+jest.mock('./../../../infrastructure/repositories/author.repository.mongo');
+const mockAuthorRepositoryPostgres = AuthorRepositoryMongo as jest.MockedClass<typeof AuthorRepositoryMongo>;
+
+describe('CreateAuthorUseCase test suite', () => {
+	let authorRepository: AuthorRepositoryMongo;
+
+	beforeEach(() => {
+		mockAuthorRepositoryPostgres.mockClear();
+	});
+
+	it('should create an author', async () => {
+		
+		authorRepository = new AuthorRepositoryMongo();
+		Container.set('AuthorRepository', authorRepository);
+
+		const validUUID = 'f2dd593e-af8e-4754-bed3-b42d2cfce636';
+		const validAuthor: AuthorRequest = {
+			name: 'My Test Author',
+			nickname: 't-nickname'
+		};
+
+		const createAuthorUseCase = Container.get(CreateAuthorUseCase);
+		await createAuthorUseCase.execute(validUUID, validAuthor);
+
+		expect(authorRepository.createAuthor).toBeCalled();
+
+	});
+
+
+});
