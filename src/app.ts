08ac2294035407e817044f89226cd81a42785796
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2021-08-03 16:25:49 +0200

    feat: added logger to app and openapi doc generator

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,6 @@
+
+import { config } from 'dotenv';
+config();
 import 'reflect-metadata';
 import Container from 'typedi';
 import express, { Application } from 'express';
@@ -14,6 +17,10 @@ import passportMiddelware from './infrastructure/middlewares/pasport';
 import { connectToDB as connectToOffensiveWordsDB } from './infrastructure/config/mongo';
 import { connectToUsersdDB } from './infrastructure/config/postgres';
 import { populateDatabase as populateUsersDatabase } from './infrastructure/config/populate';
+import expressPinoLogger from 'express-pino-logger';
+import { logger } from './infrastructure/config/logger';
+import swaggerUI from 'swagger-ui-express';
+import swaggerJsdoc, { Options } from 'swagger-jsdoc';
 
 Container.set('OffensiveWordRepository', new OffensiveWordRepositoryMongo());
 Container.set('UserRepository', new UserRepositoryPostgres());
@@ -30,9 +37,30 @@ console.log('App running');
 const app: Application = express();
 
 app.use(json());
+app.use(expressPinoLogger(logger));
 app.use(offensiveWordRouter);
 app.use(authRouter);
 app.use(passport.initialize());
 passport.use(passportMiddelware);
+
+const options: Options = {
+	definition: {
+		openapi: '3.0.0',
+		info: {
+			title: 'Blog API Rest',
+			version: '1.0.0',
+			description: 'A simple blog API '
+		},
+		servers: [
+			{
+				url: 'http://localhost:3000/api'
+			}
+		]
+	},
+	apis: ['./src/infrastructure/routes/*.ts']
+};
+
+const specs = swaggerJsdoc(options);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
 
 export default app;
