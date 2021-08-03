dcf575729dbec2cb102fb7548c49da5f34af15e3
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2021-08-03 10:48:10 +0200

    chore: created use case to create new author

---
+++ src/application/use-cases/author/create-author.use-case.ts
@@ -0,0 +1,24 @@
+import { Service } from 'typedi';
+import { Author, AuthorType } from '../../../domain/entities/author.entity';
+import { AuthorService } from '../../../domain/services/author.service';
+import { AuthorNameVO } from '../../../domain/vos/author/author-name.vo';
+import { AuthorNicknameVO } from '../../../domain/vos/author/author-nickname.vo';
+import { IdVO } from '../../../domain/vos/id.vo';
+import { IdRequest } from '../id.request';
+import { AuthorRequest } from './author.request';
+
+@Service()
+export class CreateAuthorUseCase {
+
+	constructor(private authorService: AuthorService) { }
+
+	async execute(id: IdRequest, author: AuthorRequest): Promise<void> {
+		const authorToType: AuthorType = {
+			id: IdVO.createWithUUID(id),
+			name: AuthorNameVO.create(author.name),
+			nickname: AuthorNicknameVO.create(author.nickname)
+		};
+
+		this.authorService.persist(new Author(authorToType));
+	}
+}
