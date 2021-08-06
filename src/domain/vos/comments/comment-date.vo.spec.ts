15eb2fe12b6b902ef009c284cb8a73fa0ceabb6a
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2021-08-06 23:26:26 +0200

    test: updated test to check if value object returns a string

--- src/domain/vos/comments/comment-date.vo.spec.ts
+++ src/domain/vos/comments/comment-date.vo.spec.ts
@@ -3,6 +3,6 @@ describe('CommentDateVO test suite', () => {
 	it('should create a comment date if received params are valid', () => {
 		const validDate = new Date(2020, 8, 20);
 		const commentDateTest = CommentDateVO.create(validDate);
-		expect(commentDateTest.value).toBe(validDate.getTime());
+		expect(commentDateTest.value).toBe(validDate.getTime().toString());
 	});
 });
