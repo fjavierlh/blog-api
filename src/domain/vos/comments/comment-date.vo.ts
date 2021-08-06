3c09109f152ae5e884f7132c592fa94c96437a12
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2021-08-06 23:25:31 +0200

    chore: changed value object type number to string

--- src/domain/vos/comments/comment-date.vo.ts
+++ src/domain/vos/comments/comment-date.vo.ts
@@ -2,8 +2,8 @@ export class CommentDateVO {
 
 	private constructor(private commentDate: Date) { }
 
-	get value(): number {
-		return this.commentDate.getTime();
+	get value(): string {
+		return this.commentDate.getTime().toString();
 	}
 
 	static create(commentDate: Date): CommentDateVO {
