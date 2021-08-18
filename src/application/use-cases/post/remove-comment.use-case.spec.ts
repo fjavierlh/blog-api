6bea905628e62dee37500c6b74eccc9eba757b7f
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2021-08-18 18:15:00 +0200

    test: added test to remove comment post use case

---
+++ src/application/use-cases/post/remove-comment.use-case.spec.ts
@@ -0,0 +1,20 @@
+import 'reflect-metadata';
+import Container from 'typedi';
+import { PostRepositoryMongo } from '../../../infrastructure/repositories/post.repository.mongo';
+import { RemoveCommentPostUseCase } from './remove-comment.use-case';
+jest.mock('../../../infrastructure/repositories/post.repository.mongo');
+
+describe('RemoveCommentPostUseCase', () => {
+	it('should remove a existent comment', async () => {
+		const repository = new PostRepositoryMongo();
+		Container.set('PostRepository', repository);
+		const useCase = Container.get(RemoveCommentPostUseCase);
+		const postId = '82b486d3-e592-4f04-8056-91956dfbc7f3';
+		const commentId = 'a792b2ee-7424-4f78-9a9c-3402676e80e0';
+
+		const result = await useCase.execute(postId, commentId);
+		expect(repository.deleteCommentInPost).toBeCalled();
+		expect(result).not.toBeNull();
+	});
+
+});
