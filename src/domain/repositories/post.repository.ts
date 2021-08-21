db9ef7f76487316de7f3a823318c64cb46555f11
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2021-08-21 18:13:47 +0200

    refactor: changed some name methods and name params to better naming in post repository interface

--- src/domain/repositories/post.repository.ts
+++ src/domain/repositories/post.repository.ts
@@ -6,20 +6,20 @@ export interface PostRepository {
 
     getAllPosts(): Promise<Post[]>;
 
-    getPostByID(postId: IdVO): Promise<Post>;
+    getPostByID(postID: IdVO): Promise<Post>;
     
     persistPost(post: Post): Promise<void>;
 
-    updatePost(postId: IdVO, updatedPost: Post): Promise<Post>;
+    updatePost(postID: IdVO, updatedPost: Post): Promise<Post>;
 
-    deletePostById(postId: IdVO): Promise<void>;
+    deletePostByID(postID: IdVO): Promise<void>;
 
-    saveCommentInPost(postId: IdVO, comment: CommentPost): Promise<void>;
+    saveCommentInPost(postID: IdVO, comment: CommentPost): Promise<void>;
 
-    updateCommentInPost(postId: IdVO, updatedComment: CommentPost): Promise<void>;
+    updateCommentInPost(postID: IdVO, updatedComment: CommentPost): Promise<void>;
 
-    deleteCommentInPost(postId: IdVO, commentId: IdVO): Promise<void>;
+    deleteCommentInPost(postID: IdVO, commentID: IdVO): Promise<void>;
 
-    checkIfPostExists(id: IdVO): Promise<boolean>
+    checkIfPostExists(postID: IdVO): Promise<boolean>
 
 }
