34a11c751b92f8ae72775ad55c00183f014b00fa
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2021-08-23 17:40:46 +0200

    feat: added more info to enpoints messages and added middlewares on post, put & delete routes

--- src/infrastructure/routes/post.route.ts
+++ src/infrastructure/routes/post.route.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import { body, param, validationResult } from 'express-validator';
 import passport from 'passport';
 import Container from 'typedi';
+
+import { FindAuthorByIDUseCase } from '../../application/use-cases/author/find-author-by-id.use-case';
 import { AddCommentToPostUseCase } from '../../application/use-cases/post/add-comment-to-post.use-case';
 import { CreatePostUseCase } from '../../application/use-cases/post/create-post.use-case';
 import { DeletePostUseCase } from '../../application/use-cases/post/delete-post.use-case';
@@ -11,15 +13,17 @@ import { RemoveCommentPostUseCase } from '../../application/use-cases/post/remov
 import { PostRequest } from '../../application/use-cases/post/types/post.request';
 import { UpdateCommentPostUseCase } from '../../application/use-cases/post/update-comment-post.use-case';
 import { UpdatePostUseCase } from '../../application/use-cases/post/update-post.use-case';
-import { Role } from '../../domain/vos/auth-user/role.vo';
-import { hasRole } from '../middlewares/roles.middleware';
 
+import { User } from '../../domain/entities/user.entity';
+import { Role } from '../../domain/vos/auth-user/role.vo';
+import { IdVO } from '../../domain/vos/id.vo';
+
+import { isOwner } from '../middlewares/is-owner.middleware';
+import { hasRole } from '../middlewares/roles.middleware';
 
 const router = express.Router();
 
 router.post('/api/posts',
-	body('author').isString().notEmpty(),
-	body('nickname').isString().notEmpty(),
 	body('title').isString().notEmpty(),
 	body('content').isString().notEmpty(),
 	passport.authenticate('jwt', { session: false }),
@@ -29,10 +33,16 @@ router.post('/api/posts',
 			const errors = validationResult(req);
 			if (!errors.isEmpty()) return res.status(400).json({ error: errors.array() });
 
-			const { author, nickname, title, content } = req.body;
+			const user = req.user as User;
+			const { title, content } = req.body;
+
+			const { name: author, nickname } = await getAuthor(user.id);
+
 			const newPost: PostRequest = { author, nickname, title, content };
-			const useCase = Container.get(CreatePostUseCase);
-			await useCase.execute(newPost);
+
+			const createPostUseCase = Container.get(CreatePostUseCase);
+			await createPostUseCase.execute(newPost);
+
 			res.status(201).json({ msg: 'Post created!', post: { ...newPost } });
 
 			// eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -42,31 +52,33 @@ router.post('/api/posts',
 	}
 );
 
-router.get('/api/posts', async (req: Request, res: Response) => {
+router.get('/api/posts',
+	async (req: Request, res: Response) => {
 
-	try {
-		const useCase = Container.get(FindAllPostsUseCase);
-		const allPosts = await useCase.execute();
-		res.status(200).json(allPosts);
+		try {
+			const useCase = Container.get(FindAllPostsUseCase);
+			const allPosts = await useCase.execute();
+			res.status(200).json(allPosts);
 
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	} catch (error: any) {
-		res.status(401).json({ errors: { msg: error.message } });
-	}
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		} catch (error: any) {
+			res.status(401).json({ errors: { msg: error.message } });
+		}
 
-});
+	});
 
-router.get('/api/posts/:id',
-	param('id').notEmpty().isUUID(),
+router.get('/api/posts/:postID',
+	param('postID').notEmpty().isUUID(),
 	async (req: Request, res: Response) => {
 
 		try {
 			const errors = validationResult(req);
 			if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
-			const { id } = req.params;
 
+			const { postID } = req.params;
 			const useCase = Container.get(FindPostByIdUseCase);
-			const allPosts = await useCase.execute(id);
+			const allPosts = await useCase.execute(postID);
+
 			res.status(200).json(allPosts);
 
 			// eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -77,42 +89,60 @@ router.get('/api/posts/:id',
 	}
 );
 
-router.put('/api/posts/:id',
-	param('id').notEmpty().isUUID(),
-	body('author').isString().notEmpty(),
-	body('nickname').isString().notEmpty(),
+router.put('/api/posts/:postID',
+	param('postID').notEmpty().isUUID(),
 	body('title').isString().notEmpty(),
 	body('content').isString().notEmpty(),
+	passport.authenticate('jwt', { session: false }),
+	hasRole([Role.ADMIN, Role.PUBLISHER]),
+	isOwner(),
 	async (req: Request, res: Response) => {
 
 		try {
 			const errors = validationResult(req);
 			if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
-			const { id } = req.params;
-			const { author, nickname, title, content } = req.body;
+
+			const { postID } = req.params;
+			const user = req.user as User;
+			const { name: author, nickname } = await getAuthor(user.id);
+			const { title, content } = req.body;
+
 			const updatedPost = { author, nickname, title, content };
 			const useCase = Container.get(UpdatePostUseCase);
-			await useCase.execute(id, updatedPost);
-			res.status(200).json({ statusCode: 200, message: 'Updated post success!' });
+			await useCase.execute(postID, updatedPost);
+
+			res.status(200).json({
+				statusCode: 200,
+				message: `Updated post with ID '${postID}' success`
+			});
+
 			// eslint-disable-next-line @typescript-eslint/no-explicit-any
 		} catch (error: any) {
 			res.status(401).json({ errors: { msg: error.message } });
 		}
-
 	}
 );
 
-router.delete('/api/posts/:id',
-	param('id').notEmpty().isUUID(),
+router.delete('/api/posts/:postID',
+	param('postID').notEmpty().isUUID(),
+	passport.authenticate('jwt', { session: false }),
+	hasRole([Role.ADMIN, Role.PUBLISHER]),
+	isOwner(),
 	async (req: Request, res: Response) => {
 
 		try {
 			const errors = validationResult(req);
 			if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
-			const { id } = req.params;
+
+			const { postID } = req.params;
 			const useCase = Container.get(DeletePostUseCase);
-			await useCase.execute(id);
-			res.status(200).json({ statusCode: 200, message: 'Post deleted success!' });
+			await useCase.execute(postID);
+
+			res.status(200).json({ 
+				statusCode: 200,
+				message: `Deleted post with ID '${postID}' success`
+			});
+			
 			// eslint-disable-next-line @typescript-eslint/no-explicit-any
 		} catch (error: any) {
 			res.status(401).json({ errors: { msg: error.message } });
@@ -120,67 +150,101 @@ router.delete('/api/posts/:id',
 	}
 );
 
-router.post('/api/posts/:id/comments',
-	param('id').isUUID().notEmpty(),
+router.post('/api/posts/:postID/comments',
+	param('postID').isUUID().notEmpty(),
 	body('nickname').isString().notEmpty(),
 	body('content').isString().notEmpty(),
+	passport.authenticate('jwt', { session: false }),
+	hasRole([Role.ADMIN, Role.PUBLISHER]),
 	async (req: Request, res: Response) => {
 		try {
 			const errors = validationResult(req);
 			if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
-			const { id } = req.params;
-			const { nickname, content } = req.body;
+
+			const { postID } = req.params;
+			const { content } = req.body;
+			const user = req.user as User;
+
+			const findAuthorUseCase = Container.get(FindAuthorByIDUseCase);
+			const { name: nickname } = await findAuthorUseCase.execute(user.id.value);
+
 			const newComment = { nickname, content };
 			const useCase = Container.get(AddCommentToPostUseCase);
-			await useCase.execute(id, newComment);
-			res.status(200).json({ statusCode: 200, message: 'Comment add success!' });
+			const commentID = await useCase.execute(postID, newComment);
+
+			res.status(200).json({
+				statusCode: 200,
+				message: `Save comment with ID '${commentID}' on post '${postID}' success`,
+				newComment: newComment
+			});
 
 			// eslint-disable-next-line @typescript-eslint/no-explicit-any
 		} catch (error: any) {
 			res.status(401).json({ errors: { msg: error.message } });
-
 		}
-	});
+	}
+);
 
 router.put('/api/posts/:postID/comments/:commentID',
 	param('postID').isUUID().notEmpty(),
 	param('commentID').isUUID().notEmpty(),
+	passport.authenticate('jwt', { session: false }),
+	hasRole([Role.ADMIN, Role.PUBLISHER]),
+	isOwner(),
 	async (req: Request, res: Response) => {
 		try {
 			const errors = validationResult(req);
 			if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+
 			const { postID, commentID } = req.params;
 			const { nickname, content } = req.body;
 			const updatedComment = { nickname, content };
+
 			const useCase = Container.get(UpdateCommentPostUseCase);
 			await useCase.execute(postID, commentID, updatedComment);
-			res.status(200).json({ statusCode: 200, message: 'Comment update success!' });
+
+			res.status(200).json({
+				statusCode: 200,
+				message: `Updated comment with ID '${commentID}' on post '${postID}' success`,
+				updatedComment: updatedComment 
+			});
 
 			// eslint-disable-next-line @typescript-eslint/no-explicit-any
 		} catch (error: any) {
 			res.status(401).json({ errors: { msg: error.message } });
-
 		}
-	});
+	}
+);
 
 router.delete('/api/posts/:postID/comments/:commentID',
 	param('postID').isUUID().notEmpty(),
 	param('commentID').isUUID().notEmpty(),
+	passport.authenticate('jwt', { session: false }),
+	hasRole([Role.ADMIN, Role.PUBLISHER]),
+	isOwner(),
 	async (req: Request, res: Response) => {
 		try {
 			const errors = validationResult(req);
 			if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+
 			const { postID, commentID } = req.params;
-		
+
 			const useCase = Container.get(RemoveCommentPostUseCase);
 			await useCase.execute(postID, commentID);
-			res.status(200).json({ statusCode: 200, message: 'Comment deleted success!' });
+
+			res.status(200).json({ statusCode: 200, message: `Deleted comment with ID '${commentID}' on post '${postID}' success` });
 
 			// eslint-disable-next-line @typescript-eslint/no-explicit-any
 		} catch (error: any) {
 			res.status(401).json({ errors: { msg: error.message } });
-
 		}
-	});
+	}
+);
+
+// post.route module utils
+const getAuthor = async (authorID: IdVO) => {
+	const findAuthorUseCase = Container.get(FindAuthorByIDUseCase);
+	return findAuthorUseCase.execute(authorID.value);
+};
 
 export { router as postsRouter };
