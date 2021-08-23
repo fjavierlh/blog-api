fd30a77184781762e2233ead07d259af33c6bb4e
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2021-08-23 18:02:09 +0200

    chore: added methods in author repository and service to find authors by id & check if author exsist

--- src/domain/repositories/author.repository.ts
+++ src/domain/repositories/author.repository.ts
@@ -1,7 +1,12 @@
 import { Author } from '../entities/author.entity';
+import { IdVO } from '../vos/id.vo';
 
 export interface AuthorRepository {
 
     createAuthor(author: Author): Promise<void>;
+
+    findAuthorByID(authorID: IdVO): Promise<Author>;
+
+    checkIfAuthorExists(authorID: IdVO): Promise<boolean>;
     
 }
--- src/infrastructure/repositories/author.repository.mongo.ts
+++ src/infrastructure/repositories/author.repository.mongo.ts
@@ -1,9 +1,13 @@
+import { AnyObject } from 'mongoose';
 import { Author } from '../../domain/entities/author.entity';
 import { AuthorRepository } from '../../domain/repositories/author.repository';
+import { AuthorNameVO } from '../../domain/vos/author/author-name.vo';
+import { AuthorNicknameVO } from '../../domain/vos/author/author-nickname.vo';
+import { IdVO } from '../../domain/vos/id.vo';
 import { AuthorModel } from './author.schema';
 
 export class AuthorRepositoryMongo implements AuthorRepository {
-    
+
 	async createAuthor(author: Author): Promise<void> {
 
 		const authorData = {
@@ -11,10 +15,25 @@ export class AuthorRepositoryMongo implements AuthorRepository {
 			name: author.name.value,
 			nickname: author.nickname.value
 		};
-        
-		const newAuthor = new AuthorModel(authorData);
 
-		return await newAuthor.save();
+		const newAuthor = await new AuthorModel(authorData);
+		await newAuthor.save();
+	}
+
+	async findAuthorByID(authorID: IdVO): Promise<Author> {
+		const author: AnyObject = await AuthorModel.findOne({ id: authorID.value });
+
+		const authorType = {
+			id: IdVO.createWithUUID(author.id),
+			name: AuthorNameVO.create(author.name),
+			nickname: AuthorNicknameVO.create(author.nickname)
+		};
+
+		return new Author(authorType);
+	}
+
+	async checkIfAuthorExists(authorID: IdVO): Promise<boolean> {
+		return AuthorModel.exists({ id: authorID.value });
 	}
 
 }
