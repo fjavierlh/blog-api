fdf111cd99a163b6ac28111cf66965edf29b647f
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2021-08-23 11:13:41 +0200

    refactor: deleted some manual tests in main file

--- src/app.ts
+++ src/app.ts
@@ -20,66 +20,12 @@ import { logger } from './infrastructure/config/logger';
 import swaggerUI from 'swagger-ui-express';
 import swaggerJsdoc, { Options } from 'swagger-jsdoc';
 import { PostRepositoryMongo } from './infrastructure/repositories/post.repository.mongo';
-import { AddCommentToPostUseCase } from './application/use-cases/post/add-comment-to-post.use-case';
-import { CreatePostUseCase } from './application/use-cases/post/create-post.use-case';
-import { FindAllPostsUseCase } from './application/use-cases/post/find-all-posts.use-case';
-import { FindPostByIdUseCase } from './application/use-cases/post/find-post-by-id.use-case';
-import { CommentDateVO } from './domain/vos/comments/comment-date.vo';
-import { UpdateCommentPostUseCase } from './application/use-cases/post/update-comment-post.use-case';
-import { RemoveCommentPostUseCase } from './application/use-cases/post/remove-comment.use-case';
 import { postsRouter } from './infrastructure/routes/post.route';
 
 Container.set('OffensiveWordRepository', new OffensiveWordRepositoryMongo());
 Container.set('UserRepository', new UserRepositoryPostgres());
 Container.set('AuthorRepository', new AuthorRepositoryMongo());
 Container.set('PostRepository', new PostRepositoryMongo());
-
-// Some manual test cases
-/*
-const createPostUseCase = Container.get(CreatePostUseCase);
-createPostUseCase.execute({
-	author: 'Anónimo',
-	nickname: 'anonymus',
-	title: 'An anonymus post',
-	content: 'loremimpsum'.repeat(6)
-});
-
-const findAllPostsUseCase = Container.get(FindAllPostsUseCase);
-(async () => findAllPostsUseCase.execute())().then((allPosts) => console.log('allPosts=', allPosts));
-
-const findPostByIdUseCase = Container.get(FindPostByIdUseCase);
-(async () => {
-	return findPostByIdUseCase.execute('82b486d3-e592-4f04-8056-91956dfbc7f3');
-})().then((post) => console.log('findPostByIdUseCase=', post));
-
-const addCommentUseCase = Container.get(AddCommentToPostUseCase);
-(async () => {
-	return addCommentUseCase.execute(
-		'a981b156-d8b5-4c61-9a5f-f47907aa3721',
-		{
-			nickname: 'paco1985',
-			content: 'This is a test comment',
-		});
-})();
-
-(async () => {
-	const updateCommentUseCase = Container.get(UpdateCommentPostUseCase);
-	const postId = 'a981b156-d8b5-4c61-9a5f-f47907aa3721';
-	const commentId = 'c6b06820-1717-4004-b722-b1195a1f3c39';
-	const updatedComment = {
-		nickname: 'pepejuan',
-		content: 'This is a updated comment'
-	};
-	return updateCommentUseCase.execute(postId, commentId, updatedComment);
-})().then((a) => console.log('updateCommentUseCase', a));
-
-(async () => {
-	const removeCommentPost = Container.get(RemoveCommentPostUseCase);
-	const postId = 'a981b156-d8b5-4c61-9a5f-f47907aa3721';
-	const commentId = '9374d5dd-dde2-4620-a153-493cabe1d909';
-	await removeCommentPost.execute(postId, commentId);
-})();
-*/
 
 console.log('App running');
 
